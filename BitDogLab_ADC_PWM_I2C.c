5e621655624cf1f0358f2347aff1dcca5dbf2a72
Hilquias Rodrigues de Oliveira <eaeamigo2292@gmail.com>
2025-02-13 09:49:58 -0300

    Joystick button controlling green LED

--- BitDogLab_ADC_PWM_I2C.c
+++ BitDogLab_ADC_PWM_I2C.c
@@ -5,8 +5,14 @@
 
 #define VRX_PIN 26
 #define VRY_PIN 27
+#define JOYSTICK_BUTTON 22
+#define BUTTON_A 5
+#define GREEN_LED 11
 #define BLUE_LED 12
 #define RED_LED 13
+
+static volatile uint32_t lastTimeJoystick = 0; // Tempo de última interrupção do botão do joystick
+static volatile uint32_t lastTimeA = 0;        // Tempo de última interrupção do botão A
 
 uint pwm_init_gpio(uint gpio, uint wrap)
 {
@@ -17,12 +23,59 @@ uint pwm_init_gpio(uint gpio, uint wrap)
     return slice_num;
 }
 
+bool button_debounce(volatile uint32_t *lastTime)
+{
+    uint32_t currentTime = to_us_since_boot(get_absolute_time());
+
+    // Verifica se o tempo de debouncing passou (250ms)
+    if (currentTime - *lastTime > 250000)
+    {
+        *lastTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
+void handle_button_callback(uint gpio, uint32_t events)
+{
+    if (gpio == 22 && button_debounce(&lastTimeJoystick))
+    {        
+        gpio_put(GREEN_LED, !gpio_get(GREEN_LED));
+    }
+    else if (gpio == 5 && button_debounce(&lastTimeA))
+    {
+        pwm_set_gpio_level(BLUE_LED, 0);
+        pwm_set_gpio_level(RED_LED, 0);
+    }
+}
+
+// Função para configurar a interrupção no pino do botão
+void set_interruption(int pin)
+{
+    gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_FALL, true); // Ativa interrupção para o pino
+    gpio_set_irq_callback(&handle_button_callback);      // Registra a função de callback uma vez
+    irq_set_enabled(IO_IRQ_BANK0, true);                 // Ativa interrupções no banco de GPIOs
+}
+
 int main()
 {
     stdio_init_all();
     adc_init();
     adc_gpio_init(VRX_PIN);
     adc_gpio_init(VRY_PIN);
+
+    gpio_init(JOYSTICK_BUTTON);
+    gpio_set_dir(JOYSTICK_BUTTON, GPIO_IN);
+    gpio_pull_up(JOYSTICK_BUTTON);
+    set_interruption(JOYSTICK_BUTTON);
+
+    gpio_init(BUTTON_A);
+    gpio_set_dir(BUTTON_A, GPIO_IN);
+    gpio_pull_up(BUTTON_A);
+    set_interruption(BUTTON_A);
+
+    gpio_init(GREEN_LED);
+    gpio_set_dir(GREEN_LED, GPIO_OUT);
 
     uint pwm_wrap = 4096;
     pwm_init_gpio(RED_LED, pwm_wrap);
@@ -34,7 +87,7 @@ int main()
     {
         adc_select_input(0); // Eixo X
         adc_select_input(1); // Eixo y
-        
+
         uint16_t vrx_value = adc_read();
         uint16_t vry_value = adc_read();
 
@@ -48,8 +101,9 @@ int main()
         uint32_t current_time = to_ms_since_boot(get_absolute_time());
         if (current_time - last_print_time >= 1000)
         {
-            printf("VRX: %u --- Duty Cycle RED_LED: %.2f%%\n\n", vrx_value, (vrx_value / (float)(pwm_wrap)) * 100);
-            printf("VRY: %u --- Duty Cycle BLUE_LED: %.2f%%\n\n", vry_value, (vry_value / (float)(pwm_wrap)) * 100);
+            printf("VRX: %u --- Duty Cycle RED_LED: %.2f%%\n", vrx_value, (vrx_value / (float)(pwm_wrap)) * 100);
+            printf("VRY: %u --- Duty Cycle BLUE_LED: %.2f%%\n", vry_value, (vry_value / (float)(pwm_wrap)) * 100);
+            printf("Estado do LED Verde:%s\n", gpio_get(GREEN_LED) ? "ligado" : "desligado");
             last_print_time = current_time;
         }
 
