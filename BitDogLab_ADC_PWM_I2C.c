80d2cd453334a47b14f1d4f4d0876b05dc34d8fc
Hilquias Rodrigues de Oliveira <eaeamigo2292@gmail.com>
2025-02-13 20:32:11 -0300

    Turn PWM off/on and x and y axis correction

--- BitDogLab_ADC_PWM_I2C.c
+++ BitDogLab_ADC_PWM_I2C.c
@@ -11,15 +11,16 @@
 #define BLUE_LED 12
 #define RED_LED 13
 
-static volatile uint32_t lastTimeJoystick = 0; // Tempo de última interrupção do botão do joystick
-static volatile uint32_t lastTimeA = 0;        // Tempo de última interrupção do botão A
+static volatile uint32_t last_time_joystick = 0; // Tempo de última interrupção do botão do joystick
+static volatile uint32_t last_time_A = 0;        // Tempo de última interrupção do botão A
+bool pwm_state = true;
 
 uint pwm_init_gpio(uint gpio, uint wrap)
 {
     gpio_set_function(gpio, GPIO_FUNC_PWM);
     uint slice_num = pwm_gpio_to_slice_num(gpio);
     pwm_set_wrap(slice_num, wrap);
-    pwm_set_enabled(slice_num, true);
+    pwm_set_enabled(slice_num, pwm_state);
     return slice_num;
 }
 
@@ -38,14 +39,13 @@ bool button_debounce(volatile uint32_t *lastTime)
 
 void handle_button_callback(uint gpio, uint32_t events)
 {
-    if (gpio == 22 && button_debounce(&lastTimeJoystick))
-    {        
+    if (gpio == 22 && button_debounce(&last_time_joystick))
+    {
         gpio_put(GREEN_LED, !gpio_get(GREEN_LED));
     }
-    else if (gpio == 5 && button_debounce(&lastTimeA))
+    else if (gpio == 5 && button_debounce(&last_time_A))
     {
-        pwm_set_gpio_level(BLUE_LED, 0);
-        pwm_set_gpio_level(RED_LED, 0);
+        pwm_state = !pwm_state;
     }
 }
 
@@ -85,29 +85,37 @@ int main()
 
     while (true)
     {
-        adc_select_input(0); // Eixo X
-        adc_select_input(1); // Eixo y
-
-        uint16_t vrx_value = adc_read();
-        uint16_t vry_value = adc_read();
-
-        // Ajuste da escala
-        vrx_value = (vrx_value >= 2047) ? (vrx_value - 2047) * 2 : (2048 - vrx_value) * 2; // x
-        vry_value = (vry_value >= 2047) ? (vry_value - 2047) * 2 : (2048 - vry_value) * 2; // y
-
-        pwm_set_gpio_level(RED_LED, vrx_value);
-        pwm_set_gpio_level(BLUE_LED, vry_value);
-
-        uint32_t current_time = to_ms_since_boot(get_absolute_time());
-        if (current_time - last_print_time >= 1000)
+        if (pwm_state)
         {
-            printf("VRX: %u --- Duty Cycle RED_LED: %.2f%%\n", vrx_value, (vrx_value / (float)(pwm_wrap)) * 100);
-            printf("VRY: %u --- Duty Cycle BLUE_LED: %.2f%%\n", vry_value, (vry_value / (float)(pwm_wrap)) * 100);
-            printf("Estado do LED Verde:%s\n", gpio_get(GREEN_LED) ? "ligado" : "desligado");
-            last_print_time = current_time;
-        }
+            adc_select_input(1); // Eixo X
+            uint16_t vrx_value = adc_read();
+            adc_select_input(0); // Eixo y
+            uint16_t vry_value = adc_read();
 
+            // Ajuste da escala
+            vrx_value = (vrx_value >= 2047) ? (vrx_value - 2047) * 2 : (2048 - vrx_value) * 2; // x
+            vry_value = (vry_value >= 2047) ? (vry_value - 2047) * 2 : (2048 - vry_value) * 2; // y
+
+            pwm_set_gpio_level(RED_LED, vrx_value);
+            pwm_set_gpio_level(BLUE_LED, vry_value);
+        }
+        else
+        {
+            pwm_set_gpio_level(RED_LED, 0);
+            pwm_set_gpio_level(BLUE_LED, 0);
+        }
+        
         sleep_ms(100);
     }
     return 0;
 }
+
+        // uint32_t current_time = to_ms_since_boot(get_absolute_time());
+        // if (current_time - last_print_time >= 1000)
+        // {
+        //     printf("VRX: %u --- Duty Cycle RED_LED: %.2f%%\n", vrx_value, (vrx_value / (float)(pwm_wrap)) * 100);
+        //     printf("VRY: %u --- Duty Cycle BLUE_LED: %.2f%%\n", vry_value, (vry_value / (float)(pwm_wrap)) * 100);
+        //     printf("Estado do LED Verde:%s\n", gpio_get(GREEN_LED) ? "ligado" : "desligado");
+        //     printf("Estado da PWM %s\n", pwm_state ? "ativada" : "desativada");
+        //     last_print_time = current_time;
+        // }
